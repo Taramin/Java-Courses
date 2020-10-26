b8c6c55a76c2948b7a0ffcadd6eb64ea3e4eb260
Taramin <56867929+Taramin@users.noreply.github.com>
2020-10-26 21:39:26 +0300

    Delete ExceptionIncorrectFormatSqrt.java

--- Task_2/ExceptionIncorrectFormatSqrt.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class ExceptionIncorrectFormatSqrt extends RuntimeException {
-    public ExceptionIncorrectFormatSqrt() {
-        super("корень можно извлекать только в формате sqrt(a)");
-    }
-}
