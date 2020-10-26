d9925a3df76e55bed404bf8ea381d4633d80e10e
Taramin <56867929+Taramin@users.noreply.github.com>
2020-10-26 21:39:48 +0300

    Delete ExceptionIncorrectInput.java

--- Task_2/ExceptionIncorrectInput.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class ExceptionIncorrectInput extends RuntimeException{
-    public ExceptionIncorrectInput() {
-        super("введи нормальные числа блин без букв");
-    }
-}
