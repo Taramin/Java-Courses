0389458f98b6064cef0cf49c741ef2be4b4081cb
Taramin <56867929+Taramin@users.noreply.github.com>
2020-10-26 21:38:46 +0300

    Delete ExceptionDivisionToZero.java

--- Task_2/ExceptionDivisionToZero.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class ExceptionDivisionToZero extends RuntimeException {
-    public ExceptionDivisionToZero() {
-        super("не дели на ноль, так нельзя");
-    }
-}
