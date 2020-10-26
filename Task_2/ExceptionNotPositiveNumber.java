71e5756983271b3ae61d8910a07e091964fe95b3
Taramin <56867929+Taramin@users.noreply.github.com>
2020-10-26 21:39:58 +0300

    Delete ExceptionNotPositiveNumber.java

--- Task_2/ExceptionNotPositiveNumber.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class ExceptionNotPositiveNumber extends RuntimeException {
-    public ExceptionNotPositiveNumber() {
-        super("не надо писать отрицательные числа там где нельзя");
-    }
-}
